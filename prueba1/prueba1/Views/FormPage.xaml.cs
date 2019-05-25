ef05a28b060be9b440e3b61a547232a793439b63
Carlos Catrián Gaete <flucec@hotmail.com>
2019-05-25 06:15:33 -0400

    api

--- prueba1/prueba1/Views/FormPage.xaml.cs
+++ prueba1/prueba1/Views/FormPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -17,58 +18,72 @@ namespace prueba1.Views
 			InitializeComponent ();
 		}
 
-        private void Button_Clicked(object sender, EventArgs e)
+        private async void Button_Clicked(object sender, EventArgs e)
         {
             if (string.IsNullOrEmpty(nombre.Text))
             {
-                DisplayAlert("Error", "Debe ingresar nombre.", "OK");
+                await DisplayAlert("Error", "Debe ingresar nombre.", "OK");
                 nombre.Focus();
                 return;
             }
 
             if (string.IsNullOrEmpty(rut.Text))
             {
-                DisplayAlert("Error", "Debe ingresar RUT.", "OK");
+                await DisplayAlert("Error", "Debe ingresar RUT.", "OK");
                 rut.Focus();
                 return;
             }
 
             if (string.IsNullOrEmpty(email.Text))
             {
-                DisplayAlert("Error", "Debe ingresar Email.", "OK");
+                await DisplayAlert("Error", "Debe ingresar Email.", "OK");
                 email.Focus();
                 return;
             }
 
             if (string.IsNullOrEmpty(pass1.Text))
             {
-                DisplayAlert("Error", "Debe ingresar Contraseña.", "OK");
+                await DisplayAlert("Error", "Debe ingresar Contraseña.", "OK");
                 pass1.Focus();
                 return;
             }
 
             if (pass1.Text.Length < 6)
             {
-                DisplayAlert("Error", "La Contraseña debe tener mínimo 6 caracteres.", "OK");
+                await DisplayAlert("Error", "La Contraseña debe tener mínimo 6 caracteres.", "OK");
                 pass1.Focus();
                 return;
             }
 
             if (string.IsNullOrEmpty(pass2.Text))
             {
-                DisplayAlert("Error", "Debe ingresar la Confirmacion de Contraseña.", "OK");
+                await DisplayAlert("Error", "Debe ingresar la Confirmacion de Contraseña.", "OK");
                 pass2.Focus();
                 return;
             }
 
             if (pass1.Text != pass2.Text)
             {
-                DisplayAlert("Error", "Deben coincidir las contraseñas.", "OK");
+                await DisplayAlert("Error", "Deben coincidir las contraseñas.", "OK");
                 pass1.Focus();
                 return;
             }
 
-            DisplayAlert("", "Campos Validados", "Aceptar");
+            await DisplayAlert("", "Campos Validados", "Aceptar");
+            //activo la espera 
+            //waitActivityIndicator.IsRunning = true;
+            //bloqueo el boton 
+            aceptar.IsEnabled = false;
+            HttpClient client = new HttpClient();
+            //direccion base raiz
+            client.BaseAddress = new Uri("http://service.twk.cl");
+
+            // método asincrono await espera la respuesta
+            string jsonData = "{'nombre' :'" + nombre.Text + "', 'pass' :'" + pass1.Text + "', 'rut' :'" + rut.Text + "' }";
+
+            var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            HttpResponseMessage response = await client.PostAsync("/api/alumno/179143488", content);
+
         }
     }
 }
